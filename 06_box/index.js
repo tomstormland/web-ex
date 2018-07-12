1af13db3ef7b83b90261c1b644a06b83702e4c38
tomstormland <tomstormland@gmail.com>
2018-07-12 08:18:36 +0300

    x

---
+++ 06_box/index.js
@@ -0,0 +1,1 @@
+$('#box').load('box.html');
---
+++ 06_box/server.js
@@ -0,0 +1,37 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+var sass = require('gulp-sass');
+var path = require('path');
+
+// Static Server + watching scss/html files
+gulp.task('serve', ['sass'], function() {
+
+  browserSync.init({
+    server: {
+      baseDir: "./",
+      index: "index.html",
+      routes: {
+        '/img': path.join(__dirname, '../common/img'),
+        '/lib': path.join(__dirname, '../common/lib'),
+      },
+    },
+    open: false,
+  });
+
+  gulp.watch("*.scss", ['sass']);
+  gulp.watch([
+    "*.{html,js}",
+  ]).on('change', browserSync.reload);
+});
+
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function() {
+  return gulp.src("box.scss")
+    .pipe(sass())
+    .pipe(gulp.dest("./"))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('default', ['serve']);
+
+gulp.start();
